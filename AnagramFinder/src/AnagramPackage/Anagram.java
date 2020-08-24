30a1ce4da460e648ee2357d2b741b308948487cd
uinur <uinur@10.0.0.35>
2020-08-24 16:03:23 -0400

    version 1, loading time display added.

--- AnagramFinder/src/AnagramPackage/Anagram.java
+++ AnagramFinder/src/AnagramPackage/Anagram.java
@@ -1,4 +1,4 @@
-/* Java program that prints out to the user all the anagrams to his input word. */
+/** Java program that prints out to the user all the anagrams to his input word. */
 
 package AnagramPackage;
 import java.util.ArrayList;
@@ -13,14 +13,17 @@ public class Anagram {
 	
 /*Helper Methods Implementation */	
 	//StringSorter//
-	public static String StringSorter(String str)
-	{
+	public static String StringSorter(String str) {
 		char sortArray[] = (str.toLowerCase()).toCharArray();
 		Arrays.sort(sortArray);
 		return new String(sortArray);
 	}
 	
-	//AnagramFinder//
+	/** Anagram Finder 
+	 * @params function takes in s1(the word) and HashMap<String K, String V> 
+	 * where keys are 'alphabetically sorted of the value string',
+	 * where values are words in the dictionary.
+	 * @return string, return Value from the HashMap to the Key.  */
 	public static String AnagramFinder(String s1, HashMap<String, String> dictionary)
 	{
 		String s1_sorted = StringSorter(s1); //s1_sorted in alphabetic order
@@ -30,7 +33,10 @@ public class Anagram {
 		
 /*Driver method */
 	public static void main(String[] args) throws FileNotFoundException  {
-				
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		long start = System.currentTimeMillis(); 
+		
 		//Read in file line by line and save lines in a String ArrayList. //
 		File f = new File("C:\\Users\\uinur\\Desktop\\dictionary.txt"); // need to change the filepath to filename later
 		Scanner s2 = new Scanner(f);
@@ -51,16 +57,25 @@ public class Anagram {
 			if(map.get(alphOrdered) != null) map.put(alphOrdered, map.get(alphOrdered) + ", " + dictionaryList.get(i));
 			else map.put(alphOrdered, dictionaryList.get(i));		
 		}
-	
+		
+		
 		/* Prompt to the user and save the user input as a string */
 		System.out.println("Enter a word whose anagrams you want to find:");
 		Scanner s = new Scanner(System.in);
 		String userInput = s.nextLine();
-				
+	
+		
 		/*Call AnagramFinder method on the string saved from the user input. */
 		String out = AnagramFinder(userInput, map);
+		String count[] = out.split(",");
+		
+		long end = System.currentTimeMillis(); 
+		long duration = end - start;
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		if (out != null)
 		{
+			System.out.println(count.length + " anagrams found in " + duration + " ms." );
 			System.out.println(out);
 		}
 		else System.out.println("No anagrams for " + userInput);
